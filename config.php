0269aa7a6282db790947b83680e1555932dc44bb
Tom Rochette <roctom@gmail.com>
2015-02-28 12:27:08 -0500

    Update date formatting from "jS M Y" to "M jS, Y".

--- config.php
+++ config.php
@@ -26,3 +26,4 @@ $config['site_title'] = 'tomrochette.com';
 $config['theme'] = 'tomrochette.com';
 $config['pages_order_by'] = 'date';
 $config['pages_order'] = 'desc';
+$config['date_format'] = 'M jS, Y';
