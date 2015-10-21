2d14da2a419fd155ecdc54ecb0631643eafd3bb2
Tom Rochette <roctom@gmail.com>
2015-10-20 21:42:12 -0400

    Add a small plugin to have a default template if none is given in the article.

---
+++ user/plugins/defaulttemplate/defaulttemplate.php
@@ -0,0 +1,41 @@
+<?php
+namespace Grav\Plugin;
+
+use Grav\Common\Data;
+use Grav\Common\Plugin;
+
+class DefaultTemplatePlugin extends Plugin
+{
+	/**
+	 * Activate feed plugin only if feed was requested for the current page.
+	 *
+	 * Also disables debugger.
+	 */
+	public function onPluginsInitialized()
+	{
+		if ($this->isAdmin()) {
+			$this->active = false;
+			return;
+		}
+
+		$this->enable([
+			'onTwigSiteVariables' => ['onTwigSiteVariables', 0]
+		]);
+	}
+	/**
+	 * Set needed variables to display the feed.
+	 */
+	public function onTwigSiteVariables()
+	{
+		/** @var \Grav\Common\Page\Page $page */
+		$page = $this->grav['page'];
+		$hasTemplate = isset($page->header()->template);
+		if ( $hasTemplate) {
+			return;
+		}
+
+		$default_template = $this->config->get('plugins.defaulttemplate.default_template');
+		$twig = $this->grav['twig'];
+		$twig->template = $default_template.'.html.twig';
+	}
+}
