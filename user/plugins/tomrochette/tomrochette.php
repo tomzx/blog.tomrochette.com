8a7d928d7e99c7c0bcd73a7d8dc0296a826459bc
Tom Rochette <roctom@gmail.com>
2016-11-26 13:39:51 -0500

    Add support for exporting epub.

--- user/plugins/tomrochette/tomrochette.php
+++ user/plugins/tomrochette/tomrochette.php
@@ -9,95 +9,139 @@ use tomzx\TomRochette\PandocExport;
 
 class TomRochettePlugin extends Plugin
 {
-	public function onPageInitialized()
-	{
-		if ( ! array_key_exists('pdf', $_GET)) {
-			return;
-		}
+    public function onPageInitialized()
+    {
+        if ( ! isset($_GET['format'])) {
+            return;
+        }
 
-		require_once __DIR__ . '/vendor/autoload.php';
+        $format = strtolower($_GET['format']);
+        $formats = ['pdf', 'epub'];
+        if ( ! in_array($format, $formats)) {
+            return;
+        }
 
-		/** @var \Grav\Common\Page\Page $page */
-		$page = $this->grav['page'];
+        require_once __DIR__ . '/vendor/autoload.php';
 
-		if ( ! $this->isCached($page)) {
-			$this->generatePdf($page);
-		}
+        /** @var \Grav\Common\Page\Page $page */
+        $page = $this->grav['page'];
 
-		if ( ! $this->isCached($page)) {
-			echo 'FAIL';
-			die;
-		}
+        if ( ! $this->isCached($page, $format)) {
+            if ($format === 'pdf') {
+                $this->generatePdf($page);
+            } elseif ($format === 'epub') {
+                $this->generateEpub($page);
+            }
+        }
 
-		//Saves ie https problems
-		header("Cache-Control: public");
-		header("Content-Type: application/pdf");
-		header('Pragma: private');
-		header('Expires: 0');
-		header('Content-Disposition: inline; filename="blog.tomrochette.com - ' . $page->title() . '.pdf"');
-		echo $this->getCached($page);
-		exit;
-	}
+        if ( ! $this->isCached($page, $format)) {
+            echo 'FAIL';
+            die;
+        }
 
-	/**
-	 * @param \Grav\Common\Page\Page $page
-	 * @return string
-	 */
-	private function getCachedFile(Page $page)
-	{
-		// We can't use $page->modified() as it returns the cache mtime and not the page mtime
-		$modifiedTime = filemtime($page->filePath());
-		return $page->path() . '/' . $page->slug() . '-' . $modifiedTime . '.pdf';
-	}
+        //Saves ie https problems
+        header("Cache-Control: public");
+        if ($format === 'pdf') {
+            header("Content-Type: application/pdf");
+        } elseif ($format === 'epub') {
+            header("Content-Type: application/epub");
+        }
+        header('Pragma: private');
+        header('Expires: 0');
+        header('Content-Disposition: inline; filename="blog.tomrochette.com - ' . $page->title() . '.'.$format.'"');
+        echo $this->getCached($page, $format);
+        exit;
+    }
 
-	/**
-	 * @param \Grav\Common\Page\Page $page
-	 * @return bool
-	 */
-	private function isCached(Page $page)
-	{
-		$cacheFilename = $this->getCachedFile($page);
-		return is_readable($cacheFilename);
-	}
+    /**
+     * @param \Grav\Common\Page\Page $page
+     * @param string $format
+     * @return string
+     */
+    private function getCachedFile(Page $page, $format)
+    {
+        // We can't use $page->modified() as it returns the cache mtime and not the page mtime
+        $modifiedTime = filemtime($page->filePath());
+        return $page->path() . '/' . $page->slug() . '-' . $modifiedTime . '.' . $format;
+    }
 
-	/**
-	 * @param \Grav\Common\Page\Page $page
-	 * @return int
-	 */
-	private function getCached(Page $page)
-	{
-		$cacheFilename = $this->getCachedFile($page);
-		return readfile($cacheFilename);
-	}
+    /**
+     * @param \Grav\Common\Page\Page $page
+     * @param string $format
+     * @return bool
+     */
+    private function isCached(Page $page, $format)
+    {
+        $cacheFilename = $this->getCachedFile($page, $format);
+        return is_readable($cacheFilename);
+    }
 
-	/**
-	 * @param \Grav\Common\Page\Page $page
-	 * @return int
-	 */
-	private function generatePdf(Page $page)
-	{
-		$hash = $this->getHash($page);
-		$args = [
-			'--variable=author:Tom Rochette <tom.rochette@coreteks.org>',
-			'--metadata=author:Tom Rochette',
-			'--variable=colorlinks',
-			'--number-sections',
-			'--variable=date:' . date('F j, Y', $page->date()),
-			'--variable=commit-url:https://github.com/tomzx/blog.tomrochette.com-content/blob/'.$hash.$page->url().'/'.$page->name(),
-			'--variable=commit:'.$hash,
-			'--template='.__DIR__.'/assets/latex/default.tex',
-		];
-		$pandocExporter = new PandocExport();
-		return $pandocExporter->exportFile($page->filePath(), $this->getCachedFile($page), 'markdown+lists_without_preceding_blankline', 'latex', $args);
-	}
+    /**
+     * @param \Grav\Common\Page\Page $page
+     * @param string $format
+     * @return int
+     */
+    private function getCached(Page $page, $format)
+    {
+        $cacheFilename = $this->getCachedFile($page, $format);
+        return readfile($cacheFilename);
+    }
 
-	private function getHash(Page $page)
-	{
-		$path = $page->filePath();
+    /**
+     * @param \Grav\Common\Page\Page $page
+     * @return int
+     */
+    private function generatePdf(Page $page)
+    {
+        $hash = $this->getHash($page);
+        $args = [
+            '--variable=author:Tom Rochette <tom.rochette@coreteks.org>',
+            '--metadata=author:Tom Rochette',
+            '--variable=colorlinks',
+            '--number-sections',
+            '--variable=date:' . date('F j, Y', $page->date()),
+            '--variable=commit-url:https://github.com/tomzx/blog.tomrochette.com-content/blob/' . $hash . $page->url() . '/' . $page->name(),
+            '--variable=commit:' . $hash,
+            '--template=' . __DIR__ . '/assets/latex/default.tex',
+            '--variable=geometry:margin=1in',
+        ];
+        $pandocExporter = new PandocExport();
+        return $pandocExporter->exportFile($page->filePath(), $this->getCachedFile($page, 'pdf'), 'markdown+lists_without_preceding_blankline', 'latex', $args);
+    }
 
-		$process = new Process('git log -1 --pretty=format:%h ' . $path, $page->path());
-		$process->run();
+    /**
+     * @param \Grav\Common\Page\Page $page
+     * @return int
+     */
+    private function generateEpub(Page $page)
+    {
+        $hash = $this->getHash($page);
+        $args = [
+            '--variable=author:Tom Rochette &lt;<a href="mailto:tom.rochette@coreteks.org">tom.rochette@coreteks.org</a>&gt;',
+            '--metadata=author:Tom Rochette',
+            '--variable=colorlinks',
+            '--number-sections',
+            '--variable=date:' . date('F j, Y', $page->date()),
+            '--variable=commit-url:https://github.com/tomzx/blog.tomrochette.com-content/blob/' . $hash . $page->url() . '/' . $page->name(),
+            '--variable=commit:' . $hash,
+            '--template=' . __DIR__ . '/assets/epub/default.html',
+            '--epub-stylesheet=' . __DIR__ . '/assets/epub/default.css',
+        ];
+        $pandocExporter = new PandocExport();
+        return $pandocExporter->exportFile($page->filePath(), $this->getCachedFile($page, 'epub'), 'markdown+lists_without_preceding_blankline', 'epub3', $args);
+    }
 
-		return $process->getOutput();
-	}
+    /**
+     * @param \Grav\Common\Page\Page $page
+     * @return string
+     */
+    private function getHash(Page $page)
+    {
+        $path = $page->filePath();
+
+        $process = new Process('git log -1 --pretty=format:%h ' . $path, $page->path());
+        $process->run();
+
+        return $process->getOutput();
+    }
 }
