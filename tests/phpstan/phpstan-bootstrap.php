7bb00d09262aa9a1c5e5d4e06a231a247980ff8a
Tom Rochette <roctom@gmail.com>
2026-04-07 21:29:02 -0400

    Remove grav

--- tests/phpstan/phpstan-bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php declare(strict_types=1);
-/**
- *To help phpstan dealing with LogicException in Common\User\User.php
- */
-
-define('GRAV_USER_INSTANCE', 'FLEX');
-define('GRAV_REQUEST_TIME', microtime(true));
